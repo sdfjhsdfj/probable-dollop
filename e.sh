37871cf554567f8f639e8affe5e7552ceeddc94b
duck69exe <107806061+duck69exe@users.noreply.github.com>
2022-06-30 22:08:17 +0200

    Update e.sh

--- e.sh
+++ e.sh
@@ -3,4 +3,13 @@ sudo apt-get install python3-pip xvfb -y
 Xvfb :1 -screen 0 1920x1080x24 &
 export DISPLAY=:1
 pip3 install undetected-chromedriver
-python3 meow.py
+python3 bot.py &
+python3 bot.py &
+python3 bot.py &
+python3 bot.py &
+python3 bot.py &
+python3 bot.py &
+python3 bot.py &
+python3 bot.py &
+python3 bot.py &
+python3 bot.py &
