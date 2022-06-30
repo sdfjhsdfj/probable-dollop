c9318527406bd4bcc3bab9a6a3a6110d3f362c56
duck69exe <107806061+duck69exe@users.noreply.github.com>
2022-06-30 21:00:19 +0200

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install python3-pip chromium-browser xvfb -y
+sudo apt-get install python3-pip xvfb -y
 Xvfb :1 -screen 0 1920x1080x24 &
 export DISPLAY=:1
 pip3 install undetected-chromedriver
