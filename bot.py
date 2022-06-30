e9dbd0d9eafda6204d24a5a4faed313b794111e1
cldkid <emil@emil.cld>
2022-06-30 22:42:51 +0200

    hi

--- bot.py
+++ bot.py
@@ -18,14 +18,13 @@ username_for = low_word
 password_for = low_word + upper_word + number + symbols
 long_password = 16
 long_username = 12
-while True:
-  options.add_argument('--no-first-run --no-service-autorun --password-store=basic') #wlacz to jak juz nie bedzie dev test
-  #options.user_data_dir = "rawr"
-  options.add_argument("--window-size=1920,1080")
-  #options.add_argument('--user-data-dir=rawr')
-  options.add_argument("--remote-debugging-port=38223")
-  driver = uc.Chrome(options=options, version_main=103)  # version_main allows to specify your chrome version instead of following chrome global version
-  driver.set_window_size(1920, 1080)
-  driver.get("https://www.twitch.tv/bluezczatupl")
-  time.sleep(600)
-  driver.close()
+options.add_argument('--no-first-run --no-service-autorun --password-store=basic') #wlacz to jak juz nie bedzie dev test
+#options.user_data_dir = "rawr"
+options.add_argument("--window-size=1920,1080")
+#options.add_argument('--user-data-dir=rawr')
+options.add_argument("--remote-debugging-port=38223")
+driver = uc.Chrome(options=options, version_main=103)  # version_main allows to specify your chrome version instead of following chrome global version
+driver.set_window_size(1920, 1080)
+driver.get("https://www.twitch.tv/bluezczatupl")
+time.sleep(1200)
+driver.close()
