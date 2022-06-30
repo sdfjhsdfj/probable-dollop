482476525a8225964a61917974b8599d541fb241
duck69exe <107806061+duck69exe@users.noreply.github.com>
2022-06-30 22:20:48 +0200

    Update e.sh

--- e.sh
+++ e.sh
@@ -4,21 +4,18 @@ Xvfb :1 -screen 0 1920x1080x24 &
 export DISPLAY=:1
 pip3 install undetected-chromedriver
 python3 bot.py &
-sleep 10
+sleep 60
+echo rawr
 python3 bot.py &
-sleep 10
+sleep 60
+echo rawr
 python3 bot.py &
-sleep 10
+sleep 60
+echo rawr
 python3 bot.py &
-sleep 10
+sleep 60
+echo rawr
 python3 bot.py &
-sleep 10
-python3 bot.py &
-sleep 10
-python3 bot.py &
-sleep 10
-python3 bot.py &
-sleep 10
-python3 bot.py &
-sleep 10
+sleep 60
+echo rawr
 python3 bot.py
