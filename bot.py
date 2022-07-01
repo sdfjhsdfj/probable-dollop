01b1e2d19b9045d9b6e8560dd87480903d07cc28
sdfjhsdfj <108493119+sdfjhsdfj@users.noreply.github.com>
2022-07-02 01:51:35 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,23 +8,23 @@ import json
 import os
 import requests
 from selenium.webdriver.common.by import By
-while True:
-    options = uc.ChromeOptions()
 
-    low_word = "abcdefghijklkmnopqrstuvwxyz"
-    upper_word = "ABDCEFGHIJKLMNOPQRSTUVWXYZ"
-    number = "1234567890"
-    symbols = "!@#$%&*"
-    username_for = low_word
-    password_for = low_word + upper_word + number + symbols
-    long_password = 16
-    long_username = 12
-    options.add_argument('--no-first-run --no-service-autorun --password-store=basic') #wlacz to jak juz nie bedzie dev test
-    #options.user_data_dir = "rawr"
-    options.add_argument("--window-size=1920,1080")
-    #options.add_argument('--user-data-dir=rawr')
-    options.add_argument("--remote-debugging-port=38223")
-    driver = uc.Chrome(options=options, version_main=103)  # version_main allows to specify your chrome version instead of following chrome global version
-    driver.set_window_size(1920, 1080)
-    driver.get("https://www.twitch.tv/bluezczatupl")
-    time.sleep(60)
+options = uc.ChromeOptions()
+
+low_word = "abcdefghijklkmnopqrstuvwxyz"
+upper_word = "ABDCEFGHIJKLMNOPQRSTUVWXYZ"
+number = "1234567890"
+symbols = "!@#$%&*"
+username_for = low_word
+password_for = low_word + upper_word + number + symbols
+long_password = 16
+long_username = 12
+options.add_argument('--no-first-run --no-service-autorun --password-store=basic') #wlacz to jak juz nie bedzie dev test
+#options.user_data_dir = "rawr"
+options.add_argument("--window-size=1920,1080")
+#options.add_argument('--user-data-dir=rawr')
+options.add_argument("--remote-debugging-port=38223")
+driver = uc.Chrome(options=options, version_main=103)  # version_main allows to specify your chrome version instead of following chrome global version
+driver.set_window_size(1920, 1080)
+driver.get("https://www.twitch.tv/bluezczatupl")
+time.sleep(3600)
