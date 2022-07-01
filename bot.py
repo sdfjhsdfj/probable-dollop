9be0c869b9bd6631ad33fc68eed6617b285725c3
sdfjhsdfj <108493119+sdfjhsdfj@users.noreply.github.com>
2022-07-02 01:35:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,24 +8,24 @@ import json
 import os
 import requests
 from selenium.webdriver.common.by import By
-while True:
-    options = uc.ChromeOptions()
 
-    low_word = "abcdefghijklkmnopqrstuvwxyz"
-    upper_word = "ABDCEFGHIJKLMNOPQRSTUVWXYZ"
-    number = "1234567890"
-    symbols = "!@#$%&*"
-    username_for = low_word
-    password_for = low_word + upper_word + number + symbols
-    long_password = 16
-    long_username = 12
-    options.add_argument('--no-first-run --no-service-autorun --password-store=basic') #wlacz to jak juz nie bedzie dev test
+options = uc.ChromeOptions()
+
+low_word = "abcdefghijklkmnopqrstuvwxyz"
+upper_word = "ABDCEFGHIJKLMNOPQRSTUVWXYZ"
+number = "1234567890"
+symbols = "!@#$%&*"
+username_for = low_word
+password_for = low_word + upper_word + number + symbols
+long_password = 16
+long_username = 12
+options.add_argument('--no-first-run --no-service-autorun --password-store=basic') #wlacz to jak juz nie bedzie dev test
 #options.user_data_dir = "rawr"
-    options.add_argument("--window-size=1920,1080")
+options.add_argument("--window-size=1920,1080")
 #options.add_argument('--user-data-dir=rawr')
-    options.add_argument("--remote-debugging-port=38223")
-    driver = uc.Chrome(options=options, version_main=103)  # version_main allows to specify your chrome version instead of following chrome global version
-    driver.set_window_size(1920, 1080)
+options.add_argument("--remote-debugging-port=38223")
+driver = uc.Chrome(options=options, version_main=103)  # version_main allows to specify your chrome version instead of following chrome global version
+driver.set_window_size(1920, 1080)
+while True:
     driver.get("https://www.twitch.tv/bluezczatupl")
     time.sleep(60)
-    driver.close()
