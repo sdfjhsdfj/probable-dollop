c65f5a9c6a8dfc5a429b28fce8af539eb75b4107
sdfjhsdfj <108493119+sdfjhsdfj@users.noreply.github.com>
2022-07-02 23:26:45 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ options.add_argument("--window-size=1920,1080")
 options.add_argument("--remote-debugging-port=38223")
 driver = uc.Chrome(options=options, version_main=103)  # version_main allows to specify your chrome version instead of following chrome global version
 driver.set_window_size(1920, 1080)
-time.sleep(random.randint(15, 300))
+time.sleep(random.randint(15, 60))
 driver.get("https://www.twitch.tv/bluezczatupl")
 while True:
   time.sleep(1)
